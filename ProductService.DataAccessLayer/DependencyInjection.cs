9ad0406dc756c961a0ca9696271d853e1ccf1193
M. Rivaldo Dwiputra Namare <rivaldo.namare@day1lab.app>
2025-11-12 22:04:08 +0700

    fix connection string

--- ProductService.DataAccessLayer/DependencyInjection.cs
+++ ProductService.DataAccessLayer/DependencyInjection.cs
@@ -4,8 +4,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        string connectionStringTemplate = configuration.GetConnectionString("ProductServiceDB")!;
+
+        string connectionString = connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST")!)
+            .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD")!);
+
         services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseMySQL(configuration.GetConnectionString("ProductServiceDB")!));
+        options.UseMySQL(connectionString));
 
         services.AddScoped<IProductRepository, ProductRepository>();
 
