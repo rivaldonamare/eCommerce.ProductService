d80a2798ac96014569f2605a7c5657892b4934ef
M. Rivaldo Dwiputra Namare <rivaldo.namare@day1lab.app>
2026-02-18 12:48:46 +0700

    fix mapping product

--- ProductService.BussinessLayer/Mapper/ProductToProductDTOMappingProfile.cs
+++ ProductService.BussinessLayer/Mapper/ProductToProductDTOMappingProfile.cs
@@ -4,13 +4,27 @@ public class ProductToProductDTOMappingProfile : Profile
 {
     public ProductToProductDTOMappingProfile()
     {
-        CreateMap<Product, ProductDTO>()
-            .ForMember(dest => dest.ProductID, opt => opt.MapFrom(src => src.ProductId))
-            .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.ProductName))
-            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category))
-            .ForMember(dest => dest.CategoryCode, opt => opt.MapFrom(src => src.Category.ToString()))
-            .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice))
-            .ForMember(dest => dest.QuantityInStock, opt => opt.MapFrom(src => src.QuantityInStock));
-    }
+        CreateMap<string, ProductCategory>()
+            .ConvertUsing(src =>
+                string.IsNullOrWhiteSpace(src)
+                    ? ProductCategory.Electronics
+                    : (ProductCategory)Enum.Parse(typeof(ProductCategory), src, true)
+            );
 
+        CreateMap<Product, ProductDTO>()
+           .ForMember(dest => dest.ProductID,
+               opt => opt.MapFrom(src => src.ProductId))
+
+           .ForMember(dest => dest.ProductName,
+               opt => opt.MapFrom(src => src.ProductName))
+
+           .ForMember(dest => dest.CategoryCode,
+               opt => opt.MapFrom(src => src.Category))
+
+           .ForMember(dest => dest.UnitPrice,
+               opt => opt.MapFrom(src => src.UnitPrice))
+
+           .ForMember(dest => dest.QuantityInStock,
+               opt => opt.MapFrom(src => src.QuantityInStock));
+    }
 }
